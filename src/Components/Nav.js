ccab56e5293b5b483e0996020f7a51bd3e36ffb1
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-03-24 10:56:40 +0530

    first commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Nav.css";
+import HomeBurgorDropDown from "./HomeBurgorDropDown";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="menu">
+        <HomeBurgorDropDown />
+      </div>
+      <img
+        className="nav_image"
+        src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png"
+        alt="ERROR 404"
+      />
+    </div>
+  );
+}
+
+export default Nav;
